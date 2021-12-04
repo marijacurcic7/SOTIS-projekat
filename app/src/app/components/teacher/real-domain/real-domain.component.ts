c308726d0b41c0135230296465fe1b26e24d51c3
alek98 <alek@spectra-alarm.com>
2021-12-04 20:41:06 +0100

    create real knowledge space

--- app/src/app/components/teacher/real-domain/real-domain.component.ts
+++ app/src/app/components/teacher/real-domain/real-domain.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Domain } from 'src/app/models/domain.model';
+import { DomainProblem } from 'src/app/models/domainProblem.model';
+import { DomainService } from 'src/app/services/domain.service';
 import { PythonService } from 'src/app/services/python.service';
 
 @Component({
@@ -7,17 +11,67 @@ import { PythonService } from 'src/app/services/python.service';
   styleUrls: ['./real-domain.component.css']
 })
 export class RealDomainComponent implements OnInit {
-
+  @Input() domain: Domain | undefined;
   isPythonReady = false
+  domainProblems: DomainProblem[]
+  implications: [string, string][] = [] // array of domain problems
+
   constructor(
     private pythonServce: PythonService,
+    private domainService: DomainService,
+    private snackBar: MatSnackBar,
   ) {
   }
 
   async ngOnInit() {
+  }
+
+  async ngOnChanges(changes: SimpleChanges) {
+    if (this.domain) {
+      await this.getDomainProblems()
+      await this.loadPyodide()
+
+      await this.pythonServce.createRealKnowledgeSpace(this.implications, this.domainProblems.length)
+    }
+  }
+
+  async loadPyodide() {
     await this.pythonServce.init()
     await this.pythonServce.downloadLearningSpaces()
     this.isPythonReady = true
-    await this.pythonServce.runPythonCode()
+    // await this.pythonServce.runPythonCode()
+  }
+
+  async getDomainProblems() {
+    if (!this.domain?.id) throw new Error('domain ID is missing.')
+    this.domainService.getDomainProblems(this.domain.id).subscribe(domainProblems => {
+      if (domainProblems && domainProblems.length) {
+        this.domainProblems = domainProblems
+        // convert domainProblems to implications array [[parent, child], [parent, child],...]
+        domainProblems.forEach(problem => {
+          problem.output?.forEach(child => {
+            if (problem.id) this.implications.push([problem.id, child])
+          })
+        })
+      }
+      else this.openSuccessSnackBar('Domain Problems is empty.')
+    })
+  }
+
+
+  openSuccessSnackBar(message: string): void {
+    this.snackBar.open(message, 'Dismiss', {
+      verticalPosition: 'top',
+      panelClass: ['green-snackbar'],
+      duration: 2000,
+    });
+  }
+
+  openFailSnackBar(message = 'Something went wrong.'): void {
+    this.snackBar.open(message, 'Dismiss', {
+      verticalPosition: 'top',
+      panelClass: ['red-snackbar'],
+      duration: 2000,
+    });
   }
 }
--- app/src/app/services/python.service.ts
+++ app/src/app/services/python.service.ts
@@ -18,20 +18,49 @@ export class PythonService {
   async runPythonCode() {
 
     await this.pyodide.runPython(`
-    import pandas as pd
-    import numpy as np
-    from learning_spaces.kst import iita
-    data_frame = pd.DataFrame({'a': [1, 0, 1], 'b': [0, 1, 0], 'c': [0, 1, 1]})
-    response = iita(data_frame, v=1)
-    print(response)
-    
+import pandas as pd
+from learning_spaces.kst import iita
+
+def generate_answers(num_of_questions, num_of_students, implications):
+  result = simu(items=num_of_questions, size=num_of_students, ce=0.05, lg=0.1, delta=0, imp=implications)
+  answers = result['dataset']
+  return pd.DataFrame(answers)
+
+data_frame = pd.DataFrame({'a': [1, 0, 1], 'b': [0, 1, 0], 'c': [0, 1, 1]})
+response = iita(data_frame, v=1)
+print(response)
     `)
-    
-    const response = new IitaResponse( this.pyodide.globals.get('response').toJs())
+
+    const response = new IitaResponse(this.pyodide.globals.get('response').toJs())
     console.log(response)
 
   }
 
+  async createRealKnowledgeSpace(implications: [string, string][], numOfDomainProblems: number, numOfStudents: number = 100) {
+    // numOfDomainProblems is similar to number of questions on a test
+    this.pyodide.globals.set('implications', implications)
+    this.pyodide.globals.set('num_of_domains', numOfDomainProblems)
+    this.pyodide.globals.set('num_of_students', numOfStudents)
+
+    await this.pyodide.runPython(`
+import pandas as pd
+from learning_spaces.kst import iita, simu
+
+implications = implications.to_py()
+
+def generate_answers(num_of_domains, num_of_students, implications):
+  result = simu(items=num_of_domains, size=num_of_students, ce=0.05, lg=0.1, delta=0, imp=implications)
+  answers = result['dataset']
+  return pd.DataFrame(answers)
+
+answers = generate_answers(num_of_domains, num_of_students, implications)
+response = iita(answers, v=1)
+    `)
+    const response = new IitaResponse(this.pyodide.globals.get('response').toJs())
+    console.log(response.implications)
+  }
+
+  
 
   /**
    * -------------------Initalization & loading packages methods-----------------
@@ -39,8 +68,8 @@ export class PythonService {
 
   async downloadLearningSpaces() {
     await this.pyodide.loadPackage(['micropip'])
-        // download unofficial packages
-        await this.pyodide.runPythonAsync(`
+    // download unofficial packages
+    await this.pyodide.runPythonAsync(`
         from micropip import install
         package_url = 'https://raw.githubusercontent.com/marijacurcic7/SOTIS-projekat/pyodide/learning_spaces-0.1.0-py3-none-any.whl'
         await install(package_url)
