84af991dab07bcafffe7ed0e6c72f3d7e0725f1a
alek98 <alek@spectra-alarm.com>
2021-12-21 20:30:19 +0100

    updated example

--- test-kst-python/main.py
+++ test-kst-python/main.py
@@ -1,11 +1,5 @@
-edges = [(1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4)]
-nodes = list(set([node for pair in edges for node in pair]))
-print(edges)
-for x in nodes:
-  for y in nodes:
-    for z in nodes:
-        if (x, y) in edges and (y, z) in edges:
-                if (x, z) == (1,4):
-                    try: edges.remove((x, z))
-                    except: pass
-print(edges)
+import pandas as pd
+from learning_spaces.kst import  iita_exclude_transitive
+data_frame = pd.DataFrame({'a': [1, 0, 1], 'b': [0, 1, 0], 'c': [0, 1, 1]})
+response = iita_exclude_transitive(data_frame, v=1)
+print(response)
