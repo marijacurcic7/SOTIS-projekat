887127150e2e4c4d0b58929e7dc079523de216a5
alek98 <alek@spectra-alarm.com>
2022-01-09 20:44:03 +0100

    updated model

--- app/functions/src/imsqti.ts
+++ app/functions/src/imsqti.ts
@@ -2,6 +2,7 @@ import * as functions from "firebase-functions";
 import * as admin from 'firebase-admin';
 import { User } from "./models/user.model";
 import { Test } from "./models/test.model";
+import {parse} from 'js2xmlparser'
 
 export const generateImsqti = functions.https.onCall(async (testId: string, context) => {
   // check if teacher is signed in
@@ -24,3 +25,28 @@ export const generateImsqti = functions.https.onCall(async (testId: string, cont
 function getImsQti(test:Test) {
   console.log(test)
 }
+
+export const testQti = functions.https.onRequest(async (request, response) => {
+  const assessmentItem = {
+    '@' : {
+      'xmlns' : 'http://www.imsglobal.org/xsd/imsqti_v2p0',
+      'xsi:schemaLocation': "http://www.imsglobal.org/xsd/imsqti_v2p2  http://www.imsglobal.org/xsd/qti/qtiv2p2/imsqti_v2p2p2.xsd",
+      'identifier': 'test1234',
+      'title': 'Composition of Water',
+    }, 
+    'responseDeclaration' : {
+      '@' : {
+        'identifier': 'RESPONSE',
+        'cardinality': 'multiple', 
+        'baseType': 'identifier'
+      },
+      'correctResponse': {
+        'value': ['H', 'O'],
+      }
+    }
+  }
+  const xml = parse('assessmentItem', assessmentItem)
+  console.log(xml)
+  // response.contentType('text/xml; charset=utf8').send(xml)
+  response.send('hello xml')
+})
